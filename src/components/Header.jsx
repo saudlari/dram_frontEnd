a8a67dde1186d001ef760b12e947ead24566bbac
saudlari <larissa@riseup.net>
2025-05-25 08:02:30 +0200

    styles(navbar) : import logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,28 +2,12 @@ import ConectWallet from "./buttons/ConectWallet";
 
 export default function Header() {
   return (
-    <div className="navbar bg-base-200 shadow-sm text-base-content rounded p-10" >
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block h-5 w-5 stroke-current"
-          >
-            {" "}
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>{" "}
-          </svg>
-        </button>
-      </div>
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
+    <div className="navbar bg-base-100 shadow-sm">
+    <div className="flex-1">
+    
+        <img src="/dram.jpg" alt="LogoRizoma" className="h-8" />
+   
+    </div>
       <div className="flex-none">
         <ConectWallet />      
       </div>
